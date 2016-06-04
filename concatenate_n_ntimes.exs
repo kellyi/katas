e20d5c29b9220d9a0650df6a19fb4c195657a016
Kelly Innes <kinnes@gmail.com>
2016-06-04 17:05:49 -0400

    redoing a kata in elixir

--- concatenate_n_ntimes.exs
+++ concatenate_n_ntimes.exs
@@ -2,16 +2,8 @@
 # https://programmingpraxis.com/2016/05/10/concatenate-n-n-times/
 
 defmodule Concatenate do
-    def concatenate(n) when n > 0 do
-        concat_n_ntimes(n, Integer.to_string(n), n)
-    end
-
-    defp concat_n_ntimes(0, string, _) do
-        string
-    end
-
-    defp concat_n_ntimes(remaining, string, base) do
-        concat_n_ntimes(remaining-1, string <> Integer.to_string(base), base)
+    def concatenate(n) do
+        String.duplicate(Integer.to_string(n), n)
     end
 end
 
