bcadc0f33ecea6b2b68767a05cffebe32bb2f44e
Kelly Innes <kinnes@gmail.com>
2016-07-10 12:01:15 -0400

    solve part two of advent day 2 in elixir

--- advent_day_2.exs
+++ advent_day_2.exs
@@ -2,41 +2,38 @@
 
 defmodule FindWrappingPaperAndRibbonOrder do
 
-    def solve_paper(filename) do
-        open_file(filename)
+    def solve_paper(input) do
+        open_file(input)
+        |> Enum.map(&(string_to_list(&1)))
         |> Enum.map(&(find_square_feet(&1)))
-        |> Enum.map(&(Enum.sum(&1)))
         |> Enum.sum
     end
 
-    def solve_ribbon(filename) do
-        open_file(filename)
-        |> Enum.sort(&(&1 < &2))
-        |> Enum.map(&find_ribbon_length(&1))
+    def solve_ribbon(input) do
+        open_file(input)
+        |> Enum.map(&(string_to_list(&1)))
+        |> Enum.map(&(find_ribbon_length(&1)))
         |> Enum.sum
     end
 
     defp open_file(filename) do
         {_, f} = File.read(filename)
         String.split(f, "\n", trim: true)
-        |> Enum.to_list()
-        |> Enum.map(&(split_string(&1)))
     end
 
-    defp split_string(present) do
-        String.split(present, "x")
-        |> Enum.to_list()
-        |> Enum.map(&(String.to_integer(&1)))
+    defp string_to_list(str) do
+        [a, b, c] = String.split(str, "x", trim: true)
+        [String.to_integer(a), String.to_integer(b), String.to_integer(c)]
     end
 
-    defp find_square_feet(present) do
-        [l, w, h] = present
-        [_|t] = Enum.sort(present, &(&1 > &2))
-        [2*l*w, 2*w*h, 2*h*l, Enum.reduce(t, &(&1*&2))]
+    defp find_square_feet([l,w,h]) do
+        [_|t] = Enum.sort([l,w,h], &(&1 > &2))
+        Enum.sum([2*l*w, 2*w*h, 2*h*l, Enum.reduce(t, &(&1*&2))])
     end
 
-    def find_ribbon_length([l,w,h]) do
-        l + l + w + w + (l*w*h)
+    defp find_ribbon_length([l,w,h]) do
+        [sm,md,lg] = Enum.sort([l,w,h], &(&1 < &2))
+        Enum.sum([sm, sm, md, md]) + (sm*md*lg)
     end
 
 end
