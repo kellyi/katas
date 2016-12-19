37a8b05c47273e11d630c6bba048e90ef4c41594
Kelly Innes <kinnes@gmail.com>
2016-12-18 19:04:32 -0500

    Add check for prime numbers

--- math_functions.erl
+++ math_functions.erl
@@ -1,6 +1,6 @@
 % ch 4, ex 5, 6, and 7 from programming erlang
 -module(math_functions).
--export([even/1, odd/1, filter/2, split/1, filter_split/1]).
+-export([even/1, odd/1, filter/2, split/1, filter_split/1, prime/1]).
 
 even(N) when N rem 2 == 0 -> true;
 even(_) -> false.
@@ -26,3 +26,11 @@ split([H|T], Even, Odd) ->
 
 filter_split(L) -> 
     {filter(fun math_functions:even/1, L), filter(fun math_functions:odd/1, L)}.
+
+prime(2) -> true;
+prime(N) when N < 2 -> false;
+prime(N) when N rem 2 == 0 -> false;
+prime(N) -> prime(N, 3).
+prime(N, D) when D*D > N -> true;
+prime(N, D) when N rem D == 0 -> false;
+prime(N, D) -> prime(N, D+2).
