9f35d66b5b31ee2700076dbe6035f5e6f753f533
Kelly Innes <kinnes@gmail.com>
2016-12-26 09:49:28 -0500

    Refactoring

--- new_advent_day_1.erl
+++ new_advent_day_1.erl
@@ -8,34 +8,24 @@ parse({ok, F}) ->
     string:tokens(string:strip(
         binary:bin_to_list(F), both, $\n), ", ").
 
+count_blocks(S) ->
+    {Blocks, _} = string:to_integer(string:sub_string(S, 2)),
+    Blocks.
+
+get_turn(S) -> string:sub_string(S, 1, 1).
+
 solve(L) -> solve(L, 0, 0, north).
 
 solve([], X, Y, _) -> abs(X) + abs(Y);
 
-solve([H|T], X, Y, north) ->
-    {Blocks, _} = string:to_integer(string:sub_string(H, 2)),
-    case string:sub_string(H, 1, 1) of
-        "L" -> solve(T, X - Blocks, Y, west);
-        "R" -> solve(T, X + Blocks, Y, east)
-    end;
-
-solve([H|T], X, Y, south) ->
-    {Blocks, _} = string:to_integer(string:sub_string(H, 2)),
-    case string:sub_string(H, 1, 1) of
-        "L" -> solve(T, X + Blocks, Y, east);
-        "R" -> solve(T, X - Blocks, Y, west)
-    end;
-
-solve([H|T], X, Y, west) ->
-    {Blocks, _} = string:to_integer(string:sub_string(H, 2)),
-    case string:sub_string(H, 1, 1) of
-        "L" -> solve(T, X, Y - Blocks, south);
-        "R" -> solve(T, X, Y + Blocks, north)
-    end;
-        
-solve([H|T], X, Y, east) ->
-    {Blocks, _} = string:to_integer(string:sub_string(H, 2)),
-    case string:sub_string(H, 1, 1) of
-        "L" -> solve(T, X, Y + Blocks, north);
-        "R" -> solve(T, X, Y - Blocks, south)
+solve([H|T], X, Y, Direction) ->
+    case {get_turn(H), Direction} of 
+        {"L", north} -> solve(T, X - count_blocks(H), Y, west);
+        {"R", north} -> solve(T, X + count_blocks(H), Y, east);
+        {"L", south} -> solve(T, X + count_blocks(H), Y, east);
+        {"R", south} -> solve(T, X - count_blocks(H), Y, west);
+        {"L", west} -> solve(T, X, Y - count_blocks(H), south);
+        {"R", west} -> solve(T, X, Y + count_blocks(H), north);
+        {"L", east} -> solve(T, X, Y + count_blocks(H), north);
+        {"R", east} -> solve(T, X, Y - count_blocks(H), south)
     end.
