7d9205bacb8d1102151f541348b36b9daf5ff53f
Kelly Innes <kinnes@gmail.com>
2017-12-02 00:48:52 -0500

    Day two part two

--- new_advent_day_two.exs
+++ new_advent_day_two.exs
@@ -11,6 +11,16 @@ defmodule Solve do
     |> IO.inspect
   end
 
+  def puzzle_part_two(input) do
+    {:ok, f} = File.read(input)
+    f
+    |> String.split("\n", trim: true)
+    |> Enum.map(&(convert_to_int(&1)))
+    |> Enum.flat_map(&(find_divisible(&1)))
+    |> Enum.sum
+    |> IO.inspect
+  end
+
   defp convert_to_int(str) do
     String.split(str)
     |> Enum.map(&(String.to_integer(&1)))
@@ -19,6 +29,11 @@ defmodule Solve do
   defp min_max_difference(list) do
     Enum.max(list) - Enum.min(list)
   end
+
+  defp find_divisible(list) do
+    for n <- list, d <- list, rem(n, d) == 0, n != d, do: div(n, d)
+  end
 end
 
 Solve.puzzle_part_one("2017_advent_day_two_input.txt")
+Solve.puzzle_part_two("2017_advent_day_two_input.txt")
