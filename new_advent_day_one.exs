48c322d848367e86a6034bd3e7edf93ae907e48f
Kelly Innes <kinnes@gmail.com>
2017-12-01 19:24:51 -0500

    Day one, part two in Elixir

--- new_advent_day_one.exs
+++ new_advent_day_one.exs
@@ -11,6 +11,16 @@ defmodule Solve do
     |> IO.inspect
   end
 
+  def puzzle_part_two(input) do
+    {:ok, f} = File.read(input)
+    f
+    |> String.trim_trailing
+    |> String.split("", trim: true)
+    |> Enum.map(&(String.to_integer(&1)))
+    |> alt_check_digits
+    |> IO.inspect
+  end
+
   defp check_digits([h|t]) do
     check_digits([h|t], 0, h)
   end
@@ -30,6 +40,26 @@ defmodule Solve do
   defp check_digits([_|t], acc, first) do
     check_digits(t, acc, first)
   end
+
+  defp alt_check_digits(list) do
+    alt_check_digits(list, rotate(list, div(length(list), 2)), 0, 0)
+  end
+
+  defp alt_check_digits([], _, _, acc) do
+    acc
+  end
+
+  defp alt_check_digits([h|t], list, counter, acc) do
+    case h == hd(rotate(list, counter)) do
+      true -> alt_check_digits(t, list, counter + 1, acc + h)
+      _ -> alt_check_digits(t, list, counter + 1, acc)
+    end
+  end
+
+  defp rotate(list, rotation) do
+    Enum.drop(list, rotation) ++ Enum.take(list, rotation)
+  end
 end
 
 Solve.puzzle_part_one("2017_advent_day_one_input.txt")
+Solve.puzzle_part_two("2017_advent_day_one_input.txt")
