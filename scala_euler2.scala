795d6a4e39a7a884b4acf236e46816b85807a35a
Kelly Innes <kinnes@gmail.com>
2018-07-06 18:51:14 -0400

    Redo PE 2 in Scala

--- scala_euler2.scala
+++ scala_euler2.scala
@@ -1,14 +1,24 @@
-#!/usr/bin/env scala
+type Even = Right[Nothing, Int]
+type Odd = Left[Int, Nothing]
+def Even(n: Int) = Right(n)
+def Odd(n: Int) = Left(n)
 
-def sumEvenFibo(first: Int, second: Int, accumulator: Int): Int = {
-    if (second >= 4000000) {
-        return accumulator
-    } else if (second % 2 == 0) {
-        return sumEvenFibo(second, first + second, accumulator + second)
-    } else {
-        return sumEvenFibo(second, first + second, accumulator)
-    }
+def isEven(n: Int): Boolean = (n & 1) == 0
+
+def generate(limit: Int, list: List[Int] = List.empty): List[Int] = list match {
+  case a :: rest if (a > limit) => rest
+  case a :: b :: rest => generate(limit, a + b :: a :: b :: rest)
+  case _ => generate(limit, List(2, 1))
 }
 
-println(sumEvenFibo(1, 2, 0))
+val result = generate(4000000)
+  .map {
+    case v if (isEven(v)) => Even(v)
+    case v => Odd(v)
+  }
+  .fold(0) {
+    case (acc: Int, Right(n: Int)) => acc + n
+    case (acc, _) => acc
+  }
 
+println(result)
